6b75df1613a3f32b31e65763b7bb041d27bf21e8
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2024-03-19 18:58:16 -0700

    Hamburger Menu and @Media added
    Now adapts to mobile devices and different screens

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ popup.showModal();
 closePopup.addEventListener("click", () => {
   popup.close();
 });
+
+const hambuger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".hamburger-nav-links-container");
+
+hambuger.addEventListener("click", () => {
+  hambuger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
