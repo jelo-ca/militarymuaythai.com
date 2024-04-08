35c863cefb181d6a03183ba4fc03a928e42d8492
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2024-04-08 12:16:39 -0700

    carousel fix

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
+// CAROUSEL
+
 const carouselButton = document.querySelectorAll("[data-carousel-btn]");
 
 carouselButton.forEach((button) => {
@@ -39,16 +41,27 @@ carouselButton.forEach((button) => {
     const nextSlide = slides.querySelector("[data-next]");
     const prevSlide = slides.querySelector("[data-prev]");
 
-    let newIndex = [...slides.children].indexOf(nextSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
 
     delete nextSlide.dataset.next;
     delete prevSlide.dataset.prev;
     delete activeSlide.dataset.active;
 
-    activeSlide.dataset.prev = true;
-    nextSlide.dataset.active = true;
-    slides.children[newIndex].dataset.next = true;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    let nextIndex = newIndex >= slides.children.length - 1 ? 0 : newIndex + 1;
+    let prevIndex = newIndex - 1;
+
+    if (prevIndex < 0) prevIndex = slides.children.length - 1;
+    if (prevIndex >= slides.children.length) prevIndex = 0;
+
+    console.log("prev: " + prevIndex);
+    console.log("new: " + newIndex);
+    console.log("next: " + nextIndex);
+
+    slides.children[prevIndex].dataset.prev = true;
+    slides.children[newIndex].dataset.active = true;
+    slides.children[nextIndex].dataset.next = true;
   });
 });
